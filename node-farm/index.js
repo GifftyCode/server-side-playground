57931918c6d055abe581251e0bb2e57d7ec677ef
GifftyCode <ulokangozi@yahoo.com>
2026-03-12 13:12:24 +0100

    asynchronous read/write

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,8 +1,31 @@
 const fs = require('fs')
 
+
+// Blocking synchronous way
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8")
 console.log(textIn)
 
 const textOut = `This is what we know about avocado: ${textIn} \n Created on ${Date.now()}`
 fs.writeFileSync("./txt/output.txt" , textOut)
 console.log("file Written!")
+
+//Non-blocking asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+    console.log("data1", data)
+})
+console.log("Reading file...")
+
+// Multiple asynchronous way(callback hell)
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+        console.log("data2", data2)
+        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+            console.log("data3", data3)
+
+            fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, err => {
+                console.log("File written!!")
+            })
+        })
+    })
+})
+
