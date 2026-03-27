0f542fc31775754aee107a607358814487623a6d
GifftyCode <ulokangozi@yahoo.com>
2026-03-27 15:53:46 +0100

    callbacks

--- 3-asynchronous-js/index.js
+++ 3-asynchronous-js/index.js
@@ -4,15 +4,28 @@ const superagent = require("superagent")
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}` )
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random
-`).end((err, res) => {
-    if(err) return  console.log(err.message)
+    // Callbacks
+//     superagent.get(`https://dog.ceo/api/breed/${data}/images/random
+// `).end((err, res) => {
+//     if(err) return  console.log(err.message)
     
-    console.log(res.body)
+//     console.log(res.body.message)
 
-    fs.writeFile('dog-img.txt', res.body.message, err => {
-    console.log("Random image saved to file...")
+//     fs.writeFile('dog-img.txt', res.body.message, err => {
+//     console.log("Random image saved to file...")
+// })
+// })
+
+// Promises
+superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+.then(res => {
+    console.log(res.body.message)
+
+    fs.writeFile("dog-img.txt", res.body.message, () => {
+    console.log("Random dog image saved to file...")
 })
 })
-
+.catch(err => {
+    console.log(err.message)
+})
 })
