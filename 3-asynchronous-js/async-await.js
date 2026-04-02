bc7cf7fcaa65d3d2e6fff58cd54d1d008b5d7a90
GifftyCode <ulokangozi@yahoo.com>
2026-04-02 12:32:22 +0100

    async-await

---
+++ 3-asynchronous-js/async-await.js
@@ -0,0 +1,37 @@
+const fs = require("fs")
+const superagent = require("superagent")
+
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if(err) reject("File not found...")
+
+                resolve(data)
+        })
+    })
+}
+
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, err => {
+            if(err) reject("Couldn't write to a file")
+                resolve("Sucess")
+        })
+    })
+}
+
+const getDogPics = async () => {
+    try {
+    const data = await readFilePro(`${__dirname}/dogs.txt`)
+    console.log(`Breed: ${data}` )
+
+    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+    console.log(res.body.message)
+
+    await writeFilePro("dog-img.txt", res.body.message)
+    console.log("Random dog image saved to file...")
+    } catch (err) {
+        console.log(err)
+    }
+}
+getDogPics()
