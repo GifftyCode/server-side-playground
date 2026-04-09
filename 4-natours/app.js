d01e8f6e58a324e13d4c2f3e939b10add8b45304
Intern-Frontend <intern.frontend.leadweb@gmail.com>
2026-04-09 05:35:43 +0100

    get tours

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,13 +1,23 @@
-const express = require("express")
+const fs = require('fs');
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.get("/", (req, res) => {
-    // res.status(200).send("Hello from the server side...")
-    res.status(200).json({message: "Hello from the server side", app: "Natours"})
-})
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8'),
+);
 
-const port = 3000
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-})
+  console.log(`App running on port ${port}...`);
+});
