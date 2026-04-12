b1514c157049fc8b554a904d5d1ed1b56fa19580
Intern-Frontend <intern.frontend.leadweb@gmail.com>
2026-04-12 22:48:48 +0100

    post http meethod

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,8 +3,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8'),
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'),
 );
 
 app.get('/api/v1/tours', (req, res) => {
@@ -15,6 +17,28 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  // assign allows us to create new object by merging  existing objects together
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
 });
 
 const port = 3000;
