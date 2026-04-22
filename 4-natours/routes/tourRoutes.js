39292650999ade27e7d2de07e331415fc53488c4
GifftyCode <ulokangozi@yahoo.com>
2026-04-22 05:44:02 +0100

    crud operations

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 router
   .route('/')
-  //   .get(tourController.getAllTours)
+  .get(tourController.getAllTours)
   .post(tourController.createTour);
-// router
-//   .route('/:id')
-//   .get(tourController.getTour)
-//   .patch(tourController.updateTour)
-//   .delete(tourController.deleteTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
