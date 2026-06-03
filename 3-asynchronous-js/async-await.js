e9ce8d68447266f048187644775241f666da1709
GifftyCode <ulokangozi@yahoo.com>
2026-06-03 05:46:18 +0100

    async-await updated

--- 3-asynchronous-js/async-await.js
+++ 3-asynchronous-js/async-await.js
@@ -1,37 +1,85 @@
-const fs = require("fs")
-const superagent = require("superagent")
+const fs = require("fs");
+const superagent = require("superagent");
 
-const readFilePro = file => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(file, (err, data) => {
-            if(err) reject("File not found...")
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("File not found...");
 
-                resolve(data)
-        })
-    })
-}
+      resolve(data);
+    });
+  });
+};
 
 const writeFilePro = (file, data) => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(file, data, err => {
-            if(err) reject("Couldn't write to a file")
-                resolve("Sucess")
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Couldn't write to a file");
+      resolve("Sucess");
+    });
+  });
+};
 
 const getDogPics = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`,
+    );
+    console.log(res.body.message);
+
+    await writeFilePro("dog-img.txt", res.body.message);
+    console.log("Random dog image saved to file...");
+  } catch (err) {
+    console.log(err);
+
+    throw err;
+  }
+  return "2: Ready....";
+};
+
+//  Calling promises function
+
+//1. Regular way a:
+/*
+console.log("1: I will get a dog pics!!!");
+getDogPics();
+console.log("2: Done getting dog pics!!!");
+// This is experimemnted before returning the promise value and throwing the error.
+
+//1. Regular way b:
+console.log("1: I will get a dog pics!!!");
+const x = getDogPics();
+console.log(x);
+console.log("3: Done getting dog pics!!!");
+// This is experimemnted after returning the promise value and throwing the error.
+
+*/
+
+// Using Promise so it returns the value of x as a log
+/*
+console.log("1: I will get a dog pics!!!");
+getDogPics().then((x) => {
+  console.log(x);
+  console.log("3: Done getting dog pics!!!");
+}).catch(err => {
+    console.log(err)
+)}
+
+*/
+
+// using async
+/* (async () => {
     try {
-    const data = await readFilePro(`${__dirname}/dog.txt`)
-    console.log(`Breed: ${data}` )
-
-    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-    console.log(res.body.message)
-
-    await writeFilePro("dog-img.txt", res.body.message)
-    console.log("Random dog image saved to file...")
-    } catch (err) {
-        console.log(err)
+        console.log('1: I will get a dog pics!!!')
+        const x = await getDogPics()
+        console.log(x);
+        console.log('3: Done getting dog pics!!!');
+    } catch {
+        console.log('Error!!!');
     }
-}
-getDogPics()
+}) ();
+
+*/
