b0f7639c4e982e124ba12881e4e6b41aed84b73a
GifftyCode <ulokangozi@yahoo.com>
2026-06-12 05:36:45 +0100

    updated routes

--- 4-natours/routes/tourRoutesFS.js
+++ 4-natours/routes/tourRoutesFS.js
@@ -2,7 +2,13 @@ const express = require('express');
 const tourController = require('../controllers/tourContollerFS');
 const router = express.Router();
 
-router.param('id', tourController.checkId);
+// router.param('id', tourController.checkId);
+
+// Param Middleware - Middleware that only runs for certain parameter in our URL
+router.param('id', (req, res, next, val) => {
+  console.log(`Tour id: ${val}`);
+  next();
+});
 
 router
   .route('/')
